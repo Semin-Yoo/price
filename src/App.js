5dff975847f99e2c1fa7c3df83ef76d98551a623
Semin-Yoo <sarah0727@berkeley.edu>
2021-12-29 15:37:22 +0900

    time

--- src/App.js
+++ src/App.js
@@ -23,22 +23,22 @@ function App() {
     axios
       .all([
         axios.get(
-          "https://cors.bridged.cc/https://www.bw.com/api/data/v1/ticker?marketId=4326"
+          "https://cors-anywhere.herokuapp.com/https://www.bw.com/api/data/v1/ticker?marketId=4326"
         ),
         axios.get(
-          "https://cors.bridged.cc/https://api.upbit.com/v1/ticker?markets=KRW-BTC"
+          "https://cors-anywhere.herokuapp.com/https://api.upbit.com/v1/ticker?markets=KRW-BTC"
         ),
         axios.get(
-          "https://cors.bridged.cc/https://api.upbit.com/v1/ticker?markets=KRW-ETH"
+          "https://cors-anywhere.herokuapp.com/https://api.upbit.com/v1/ticker?markets=KRW-ETH"
         ),
         axios.get(
-          "https://cors.bridged.cc/https://api.upbit.com/v1/ticker?markets=KRW-XRP"
+          "https://cors-anywhere.herokuapp.com/https://api.upbit.com/v1/ticker?markets=KRW-XRP"
         ),
         axios.get(
-          "https://cors.bridged.cc/https://api.binance.com/api/v1/ticker/allPrices"
+          "https://cors-anywhere.herokuapp.com/https://api.binance.com/api/v1/ticker/allPrices"
         ),
         axios.get(
-          "https://cors.bridged.cc/https://exchange.jaeheon.kr:23490/query/USDKRW"
+          "https://cors-anywhere.herokuapp.com/https://exchange.jaeheon.kr:23490/query/USDKRW"
         ),
       ])
 
@@ -61,7 +61,7 @@ function App() {
     getData();
     setInterval(async () => {
       getData();
-    }, 1000000);
+    }, 500000000);
   }, []);
 
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
